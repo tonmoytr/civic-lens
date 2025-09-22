a78a832d4fa43473977ad3834cbe6d5fdf258903
tonmoytr <tonmoytr0110@gmail.com>
2025-09-22 18:31:26 +0600

    contact us page created

---
+++ app/(pages)/contact-us/page.js
@@ -0,0 +1,14 @@
+import Footer from '@/app/components/shared/Footer'
+import Navbar from '@/app/components/shared/Navbar'
+
+export default function ContactUs() {
+  return (
+    <div>
+      <Navbar/>
+      <main>
+
+      </main>
+      <Footer/>
+    </div>
+  )
+}
