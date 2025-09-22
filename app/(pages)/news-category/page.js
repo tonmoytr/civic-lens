d68395f3acd379b1f2f65f5283527456946c2233
tonmoytr <tonmoytr0110@gmail.com>
2025-09-22 10:59:57 +0600

    category title component created

--- app/(pages)/news-category/page.js
+++ app/(pages)/news-category/page.js
@@ -1,9 +1,15 @@
-import Navbar from '@/app/components/shared/Navbar'
+import CategoryPageTitle from "@/app/components/category/CategoryPageTitle";
+import Footer from "@/app/components/shared/Footer";
+import Navbar from "@/app/components/shared/Navbar";
 
 export default function CategoryPage() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <main>
+        <CategoryPageTitle />
+      </main>
+      <Footer />
     </div>
-  )
+  );
 }
