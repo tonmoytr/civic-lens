c0a54402728eb30b046fa591c7dce00a3f3a2056
tonmoytr <tonmoytr0110@gmail.com>
2025-09-22 18:30:24 +0600

    podcast page completed

---
+++ app/(pages)/podcast/page.js
@@ -0,0 +1,17 @@
+import PodcastsPageTitle from "@/app/components/podcasts/PodcastPageTitle";
+import PodcastGrid from "@/app/components/podcasts/PodcastsGrid";
+import Footer from "@/app/components/shared/Footer";
+import Navbar from "@/app/components/shared/Navbar";
+
+export default function PodcastsPage() {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <PodcastsPageTitle />
+        <PodcastGrid />
+      </main>
+      <Footer />
+    </div>
+  );
+}
---
+++ app/components/podcasts/PodcastPageTitle.jsx
@@ -0,0 +1,10 @@
+// app/components/CategoryPageTitle.jsx
+export default function PodcastsPageTitle() {
+  return (
+    <section className="page-title">
+      <div className="container">
+        <h1 className="main-title center">News Categories</h1>
+      </div>
+    </section>
+  );
+}
