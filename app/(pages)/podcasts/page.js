7cc974678ccd44799be28e7da5505cc9c2f1b5a3
tonmoytr <tonmoytr0110@gmail.com>
2025-09-22 17:27:11 +0600

    podcasts page created

---
+++ app/(pages)/podcasts/page.js
@@ -0,0 +1,14 @@
+import Footer from '@/app/components/shared/Footer'
+import Navbar from '@/app/components/shared/Navbar'
+
+export default function PodcastsPage() {
+  return (
+    <div>
+      <Navbar/>
+      <main>
+
+      </main>
+      <Footer/>
+    </div>
+  )
+}
