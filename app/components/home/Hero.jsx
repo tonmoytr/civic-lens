7cbed41ad71eaf34a2ffe81244b33d8e40876aba
tonmoytr <tonmoytr0110@gmail.com>
2025-09-23 10:35:46 +0600

    navbar fixed

--- app/components/home/Hero.jsx
+++ app/components/home/Hero.jsx
@@ -193,7 +193,7 @@ export default function Hero() {
               <div role="listitem" className="hero-category-item w-dyn-item">
                 <a
                   data-w-id="111bf94c-bd8e-eb26-f835-5daff8a9b32e"
-                  href="/news-categories/business"
+                  href="/news/business"
                   className="category-link w-inline-block"
                 >
                   <img
@@ -219,7 +219,7 @@ export default function Hero() {
               <div role="listitem" className="hero-category-item w-dyn-item">
                 <a
                   data-w-id="111bf94c-bd8e-eb26-f835-5daff8a9b32e"
-                  href="/news-categories/health-news"
+                  href="/news/technology"
                   className="category-link w-inline-block"
                 >
                   <img
@@ -230,7 +230,7 @@ export default function Hero() {
                     className="hero-category-image"
                   />
                   <div className="category-name">
-                    <div className="hero-category-name">Health News</div>
+                    <div className="hero-category-name">Technology</div>
                     <img
                       src="https://cdn.prod.website-files.com/67372e014977be3ffcac2486/679723e67cb4d6ccd3aeeb7e_ic-arrow-white.svg"
                       loading="lazy"
@@ -245,7 +245,7 @@ export default function Hero() {
               <div role="listitem" className="hero-category-item w-dyn-item">
                 <a
                   data-w-id="111bf94c-bd8e-eb26-f835-5daff8a9b32e"
-                  href="/news-categories/world-news"
+                  href="/news/world-news"
                   className="category-link w-inline-block"
                 >
                   <img
@@ -271,7 +271,7 @@ export default function Hero() {
               <div role="listitem" className="hero-category-item w-dyn-item">
                 <a
                   data-w-id="111bf94c-bd8e-eb26-f835-5daff8a9b32e"
-                  href="/news-categories/sports"
+                  href="/news/sports"
                   className="category-link w-inline-block"
                 >
                   <img
@@ -297,7 +297,7 @@ export default function Hero() {
               <div role="listitem" className="hero-category-item w-dyn-item">
                 <a
                   data-w-id="111bf94c-bd8e-eb26-f835-5daff8a9b32e"
-                  href="/news-categories/culture"
+                  href="/news/culture"
                   className="category-link w-inline-block"
                 >
                   <img
--- app/components/shared/Navbar.jsx
+++ app/components/shared/Navbar.jsx
@@ -25,145 +25,78 @@ export default function Navbar() {
                 >
                   <img
                     loading="lazy"
-                    src="https://cdn.prod.website-files.com/67372e014977be3ffcac2486/679325bd8ae35424b3781a97_kivzo.svg"
+                    style={{ width: "100px" }}
+                    src="/assets/images/logo/logo-2.png"
                     alt="Kivzo"
                   />
                 </Link>
-
-                <div className="nav-menu-wrap">
-                  <nav role="navigation" className="nav-menu w-nav-menu">
-                    <div className="w-dyn-list">
-                      <div role="list" className="nav-link-list w-dyn-items">
-                        <div
-                          role="listitem"
-                          className="nav-link-item w-dyn-item"
+              </div>
+              {/* RIGHT : Menus */}
+              <div className="nav-menu-wrap">
+                <nav role="navigation" className="nav-menu w-nav-menu">
+                  <div className="w-dyn-list">
+                    <div role="list" className="nav-link-list w-dyn-items">
+                      <div role="listitem" className="nav-link-item w-dyn-item">
+                        <div className="dot"></div>
+                        <Link
+                          href="/news/business"
+                          className="nav-link w-inline-block"
                         >
-                          <div className="dot"></div>
-                          <Link
-                            href="/news-categories/business"
-                            className="nav-link w-inline-block"
-                          >
-                            <div className="link-text">Business</div>
-                            <div className="link-hover-text">Business</div>
-                          </Link>
-                        </div>
-
-                        <div
-                          role="listitem"
-                          className="nav-link-item w-dyn-item"
+                          <div className="link-text">Business</div>
+                          <div className="link-hover-text">Business</div>
+                        </Link>
+                      </div>
+                      <div role="listitem" className="nav-link-item w-dyn-item">
+                        <div className="dot"></div>
+                        <Link
+                          href="/news/world-news"
+                          className="nav-link w-inline-block"
                         >
-                          <div className="dot"></div>
-                          <Link
-                            href="/news-categories/health-news"
-                            className="nav-link w-inline-block"
-                          >
-                            <div className="link-text">Health News</div>
-                            <div className="link-hover-text">Health News</div>
-                          </Link>
-                        </div>
+                          <div className="link-text">World News</div>
+                          <div className="link-hover-text">World News</div>
+                        </Link>
+                      </div>
 
-                        <div
-                          role="listitem"
-                          className="nav-link-item w-dyn-item"
+                      <div role="listitem" className="nav-link-item w-dyn-item">
+                        <div className="dot"></div>
+                        <Link
+                          href="/news/sports"
+                          className="nav-link w-inline-block"
                         >
-                          <div className="dot"></div>
-                          <Link
-                            href="/news-categories/world-news"
-                            className="nav-link w-inline-block"
-                          >
-                            <div className="link-text">World News</div>
-                            <div className="link-hover-text">World News</div>
-                          </Link>
-                        </div>
+                          <div className="link-text">Sports</div>
+                          <div className="link-hover-text">Sports</div>
+                        </Link>
+                      </div>
 
-                        <div
-                          role="listitem"
-                          className="nav-link-item w-dyn-item"
+                      <div role="listitem" className="nav-link-item w-dyn-item">
+                        <div className="dot"></div>
+                        <Link
+                          href="/news/culture"
+                          className="nav-link w-inline-block"
                         >
-                          <div className="dot"></div>
-                          <Link
-                            href="/news-categories/sports"
-                            className="nav-link w-inline-block"
-                          >
-                            <div className="link-text">Sports</div>
-                            <div className="link-hover-text">Sports</div>
-                          </Link>
-                        </div>
+                          <div className="link-text">Culture</div>
+                          <div className="link-hover-text">Culture</div>
+                        </Link>
+                      </div>
 
-                        <div
-                          role="listitem"
-                          className="nav-link-item w-dyn-item"
+                      <div role="listitem" className="nav-link-item w-dyn-item">
+                        <div className="dot"></div>
+                        <Link
+                          href="/authors"
+                          className="nav-link w-inline-block"
                         >
-                          <div className="dot"></div>
-                          <Link
-                            href="/news-categories/culture"
-                            className="nav-link w-inline-block"
-                          >
-                            <div className="link-text">Culture</div>
-                            <div className="link-hover-text">Culture</div>
-                          </Link>
-                        </div>
-
-                        <div
-                          role="listitem"
-                          className="nav-link-item w-dyn-item"
-                        >
-                          <div className="dot"></div>
-                          <Link
-                            href="/subscribe"
-                            className="nav-link w-inline-block"
-                          >
-                            <div className="link-text">Subscribe</div>
-                            <div className="link-hover-text">Subscribe</div>
-                          </Link>
-                        </div>
+                          <div className="link-text">Authors</div>
+                          <div className="link-hover-text">Authors</div>
+                        </Link>
                       </div>
                     </div>
+                  </div>
 
-                    {/* trailing dot before cart like OG */}
-                    <div className="dot"></div>
+                  {/* trailing dot before cart like OG */}
+                  <div className="dot"></div>
 
-                    {/* Cart button (static count for now) */}
-                    <div className="cart-button">
-                      <Link
-                        href="#"
-                        aria-label="Open cart"
-                        className="w-commerce-commercecartopenlink cart-btn w-inline-block"
-                        role="button"
-                      >
-                        <div className="w-inline-block">Cart</div>
-                        <div className="cart-item">
-                          <div>(</div>
-                          <div className="w-commerce-commercecartopenlinkcount cart-qnt">
-                            0
-                          </div>
-                          <div>)</div>
-                        </div>
-                      </Link>
-                    </div>
-                  </nav>
-                </div>
-              </div>
-
-              {/* RIGHT: Search (orange round button intentionally removed) */}
-              <div className="nav-right">
-                <form action="/search" className="search w-form" role="search">
-                  <input
-                    className="search-input w-input"
-                    maxLength={256}
-                    name="query"
-                    placeholder="Search here"
-                    type="search"
-                    id="search"
-                    required
-                  />
-                  <input
-                    type="submit"
-                    className="search-button w-button"
-                    value=""
-                    aria-label="Submit search"
-                  />
-                </form>
+                  {/* Cart button (static count for now) */}
+                </nav>
               </div>
             </div>
           </div>
